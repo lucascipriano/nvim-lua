90850fe454c491de52c3490a85dc8451a30b3c03
Lucas Cipriano <lucasncborges@gmail.com>
2022-04-04 09:30:23 -0300

    add lsp

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,4 +2,4 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true}
 vim.g.mapleader = ' '
 
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>f', ':NvimTreeToggle<CR>', opts)
